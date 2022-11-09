ce798f1b2f73247b680145f92b543f19b0d88fe1
LightningYTlol <unconfigured@null.spigotmc.org>
2022-11-08 21:12:35 -0800

    added splash screen

--- main.js
+++ main.js
@@ -2,21 +2,46 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path')
 const url = require('url')
 
-let win;
+
 
 function createWindow(){
-    win = new BrowserWindow({width:800, height:600})
-
-    win.loadFile('index.html')
-
-    win.webContents.openDevTools()
-
-    win.on('closed', () => {
-        win=null;
+    const mainWindow = new BrowserWindow({
+        width:800,
+        height:600,
+        show: false
     })
+
+    mainWindow.loadFile('index.html')
+
+    var splash = new BrowserWindow({
+        width: 500,
+        height: 300,
+        transparent: true,
+        frame: false,
+        alwaysOnTop: true
+    });
+
+    splash.loadFile('views/splash/splash.html');
+    splash.center();
+    setTimeout(function () {
+        splash.close();
+        mainWindow.center();
+        mainWindow.show();
+    }, 5000);
+
+
+
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+        // On macOS, it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
 
 app.on('window-all-closed', () => {
     if(process.platform !== 'darwin'){

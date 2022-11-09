ee86738db6d9951abc0e6c81ff4dcf3a8c173ec3
LightningYTlol <unconfigured@null.spigotmc.org>
2022-11-08 20:45:53 -0800

    Create base files and change background color.

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path')
+const url = require('url')
+
+let win;
+
+function createWindow(){
+    win = new BrowserWindow({width:800, height:600})
+
+    win.loadFile('index.html')
+
+    win.webContents.openDevTools()
+
+    win.on('closed', () => {
+        win=null;
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+})
